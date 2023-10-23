d5d1d19817660370b18f8d70c64d82f5b24600a7
Ryan Kim <ryankim@Ryans-MBP-2.lan>
2023-10-22 23:10:28 -0700

    simplify useIsFirstRender

--- react/3_useIsFirstRender.tsx
+++ react/3_useIsFirstRender.tsx
@@ -1,17 +1,11 @@
 import { useRef, useEffect } from 'react'
 
 export function useIsFirstRender(): boolean {
-  const renderRef = useRef(false);
-
-  useEffect(() => {
-    if (!renderRef.current) {
-      renderRef.current = true;
-    }
-  }, [])
-
+  const renderRef = useRef(true);
+  
   if (renderRef.current) {
-    return false;
-  } else {
     return true;
   }
+
+  return false;
 }
