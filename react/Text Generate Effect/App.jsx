47f0d144782d39a993657f057e35670068dd9e15
Ryan Kim <ryankim@Ryans-MBP-2.lan>
2023-12-01 00:20:31 -0800

    Text Generate Effect

---
+++ react/Text Generate Effect/App.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+import TextGenerate from './TextGenerate';
+
+export default function App() {
+  return (
+    <div>
+      <TextGenerate />
+    </div>
+  );
+}
