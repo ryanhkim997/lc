79efc664c558c905d97abfa66a61485edd2f3c3c
Ryan Kim <ryankim@Ryans-MBP-2.lan>
2023-12-02 17:51:35 -0800

    Multiple Filters

---
+++ react/Multiple Filters/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import MultipleFilters from "./MultipleFilters";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <MultipleFilters />
+    </div>
+  );
+}
---
+++ react/Multiple Filters/MultipleFIlters.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { items as defaultItems } from "./items";
+import "./style.css";
+
+export default function MultipleFilters() {
+  const [items, setItems] = useState(defaultItems);
+  const [activeFilters, setActiveFilters] = useState([]);
+
+  function handleClick(filter) {
+    if (activeFilters.includes(filter)) {
+      const filters = activeFilters.filter((e) => e !== filter);
+      setActiveFilters(filters);
+    } else {
+      setActiveFilters((prevState) => [...prevState, filter]);
+    }
+  }
+
+  function handleFilter() {
+    if (activeFilters.length < 1) {
+      setItems(defaultItems);
+    } else {
+      const filteredItems = [];
+      activeFilters.forEach((filter) => {
+        defaultItems.forEach((item) => {
+          if (item.category === filter) {
+            filteredItems.push(item);
+          }
+        });
+      });
+      console.log(filteredItems);
+      setItems(filteredItems);
+    }
+  }
+
+  useEffect(() => {
+    handleFilter();
+  }, [activeFilters]);
+
+  let filters = ["Bags", "Watches", "Sports", "Sunglasses"];
+
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Algochurn Filters</h2>
+      <div className="buttons-container">
+        {filters.map((filter, idx) => (
+          <button
+            className={`button ${activeFilters.includes(filter) && "active"}`}
+            key={`filters-${idx}`}
+            onClick={() => {
+              handleClick(filter);
+            }}
+          >
+            {filter}
+          </button>
+        ))}
+      </div>
+      <div className="items-container">
+        {items.map((item, idx) => (
+          <div key={`items-${idx}`} className="item">
+            <p>{item.name}</p>
+            <p className="category">{item.category}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
